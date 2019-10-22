024f24840dd5415ed1bf1c6276659fb7f8dc154f
David Wu <dwu97@yahoo.com>
2019-10-21 22:47:22 -0400

    Small fix

--- baystateMarathonProjectJS.js
+++ baystateMarathonProjectJS.js
@@ -1,5 +1,5 @@
 $("#detailsPopover1").popover();
-$("#detailsPopover1").attr("data-content", "Modified Abdominal Assault: 1) Sit-ups, 2) Russian Twists, 3) Ankle biters, 4) Rocky sit-ups, 5) Cocoons, 6) Side plank (left and right), 7) Bottoms up, 8) Flutter kicks");
+$("#detailsPopover1").attr("data-content", "1) Sit-ups, 2) Russian Twists, 3) Ankle biters, 4) Rocky sit-ups, 5) Cocoons, 6) Side plank (left and right), 7) Bottoms up, 8) Flutter kicks");
 
 $("#detailsPopover2").popover();
-$("#detailsPopover2").attr("data-content", "Modified Payne Abs: 1) Flutter Kicks, 2) Scissors, 3) Crunches, 4) Russian Twists, 5) Crunches, 6) Rockers, 7) Crunches, 8) Supermans");
+$("#detailsPopover2").attr("data-content", "1) Flutter Kicks, 2) Scissors, 3) Crunches, 4) Russian Twists, 5) Crunches, 6) Rockers, 7) Crunches, 8) Supermans");
